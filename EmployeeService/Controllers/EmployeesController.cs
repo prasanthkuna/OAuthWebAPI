3e3894788588cb77cd54e7167ee7d755656b0e9e
prasanthkuna <prasanthkuna@gmail.com>
2020-03-26 16:42:28 +0530

    oauth webapi user login

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeEntities db = new EmployeeEntities())
+            {
+                return db.tblEmployees.Select(x => new Employee
+                {
+                    FirstName = x.FirstName,
+                    Gender = x.Gender,
+                    Id = x.ID,
+                    HireDate = x.HireDate,
+                    JobTitle = x.JobTitle,
+                    LastName = x.LastName,
+                    Salary = x.Salary,
+                    WebSite = x.WebSite
+                }).ToList();
+            }
+        }
+    }
+}
---
+++ EmployeeService/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeService.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string JobTitle { get; set; }
+        public string WebSite { get; set; }
+        public int? Salary { get; set; }
+        public DateTime? HireDate { get; set; }
+    }
+}
